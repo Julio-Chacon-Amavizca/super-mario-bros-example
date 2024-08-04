ce80d7542dc45e2a04dda4253106fde865e4ded0
JulioChacon <jcchacon3000@gmail.com>
2024-08-04 01:45:19 -0700

    spritesheet component added.

---
+++ spritesheet.js
@@ -0,0 +1,39 @@
+const INIT_SPRITESHEET = [
+  {
+    key: 'mario',
+    path: 'assets/entities/mario.png',
+    frameWidth: 18,
+    frameHeight: 16
+  },
+  {
+    key: 'mario-grown',
+    path: 'assets/entities/mario-grown.png',
+    frameWidth: 18,
+    frameHeight: 32
+  },
+  {
+    key: 'goomba',
+    path: 'assets/entities/overworld/goomba.png',
+    frameWidth: 16,
+    frameHeight: 16
+  },
+  {
+    key: 'coin',
+    path: 'assets/collectibles/coin.png',
+    frameWidth: 16,
+    frameHeight: 16
+  },
+  {
+    key: 'mushroom',
+    path: 'assets/collectibles/mushroom.png',
+    frameWidth: 16,
+    frameHeight: 16
+  }
+]
+
+export const initSpritesheet = ({ load }) => {
+  INIT_SPRITESHEET.forEach(({ key, path, frameWidth, frameHeight }) => {
+    load.spritesheet(key, path, { frameWidth, frameHeight })
+  }
+  )
+}
